ffc237f6cc283184247369d7c47b562026c232e6
Florent BOURASSE <fbourasse@jahia.com>
2018-08-20 12:01:54 +0200

    Display of the missing dependencies in a Karaf table

--- src/main/java/org/jahia/modules/dependenciesanalyzer/jcrcommands/PrintMissingDependenciesCommand.java
+++ src/main/java/org/jahia/modules/dependenciesanalyzer/jcrcommands/PrintMissingDependenciesCommand.java
@@ -5,6 +5,8 @@ import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.api.console.Session;
+import org.apache.karaf.shell.support.table.Col;
+import org.apache.karaf.shell.support.table.ShellTable;
 import org.jahia.modules.dependenciesanalyzer.services.Utils;
 
 @Command(scope = "utils", name = "print-missing-dependencies", description = "Print missing dependencies")
@@ -16,7 +18,15 @@ public class PrintMissingDependenciesCommand implements Action {
 
     @Override
     public Object execute() throws Exception {
-        Utils.getDependenciesAnalyzerService().printDependenciesAnalysesList();
+        final ShellTable table = new ShellTable();
+        table.column(new Col("Module"));
+        table.column(new Col("Dependency type"));
+        table.column(new Col("Dependency"));
+        Utils.getDependenciesAnalyzerService().printDependenciesAnalysesList().forEach((line) -> {
+            table.addRow().addContent(line.split(";"));
+        });
+
+        table.print(System.out, true);
         return null;
     }
 }
